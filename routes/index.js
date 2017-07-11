2e5c9ebef4a2aecd3f5ccdbbc1a82853fc61136a
sefi <sefi@tikalk.com>
2017-07-11 10:11:22 +0300

    stateful API

--- routes/index.js
+++ routes/index.js
@@ -4,147 +4,147 @@ const dGenUtils = require('../src/dungeon-generator');
 const fs = require('fs');
 
 module.exports = (passport) => {
-    /* GET home page. */
+	/* GET home page. */
 
-    const authCallbackObj = {
-        successRedirect: '/start',
-        failureRedirect: '/'
-    };
+	const authCallbackObj = {
+		successRedirect: '/start',
+		failureRedirect: '/'
+	};
 
-    const userErrorHandler = (req, res) => err => {
-        console.log(`[${req.user.tikalId}]: ${err}`);
-        res.sendStatus(400);
-    };
+	const userErrorHandler = (req, res) => err => {
+		console.log(`[${req.user.tikalId}]: ${err}`);
+		res.sendStatus(400);
+	};
 
-    const mazeErrorHandler = (req, res) => err => {
-        console.log(`[${req.params.mazeId}]: ${err}`);
-        res.status(403).send({error: err.message});
-    };
+	const mazeErrorHandler = (req, res) => err => {
+		console.log(`[${req.params.mazeId}]: ${err}`);
+		res.status(403).send({error: err.message});
+	};
 
-    router.get('/', (req, res, next) => {
-        res.render('index');
-    });
+	router.get('/', (req, res, next) => {
+		res.render('index');
+	});
 
-    router.get('/start', isLoggedIn, (req, res, next) => {
-        res.render('start');
-    });
+	router.get('/start', isLoggedIn, (req, res, next) => {
+		res.render('start');
+	});
 
-    router.get('/timi', isLoggedIn, (req, res, next) => {
-        dGenUtils.getClue(req.user)
-            .then((resClue) => {
-                res.send(resClue.clue[1]);
-            })
-            .catch(userErrorHandler(req, res));
-    });
+	router.get('/timi', isLoggedIn, (req, res, next) => {
+		dGenUtils.getClue(req.user)
+			.then((resClue) => {
+				res.send(resClue.clue[1]);
+			})
+			.catch(userErrorHandler(req, res));
+	});
 
-    router.get('/start-clue', isLoggedIn, (req, res, next) => {
-        dGenUtils.getClue(req.user)
-            .then((resClue) => {
-                res.send(resClue.clue[2]);
-            })
-            .catch(userErrorHandler(req, res));
-    });
+	router.get('/start-clue', isLoggedIn, (req, res, next) => {
+		dGenUtils.getClue(req.user)
+			.then((resClue) => {
+				res.send(resClue.clue[2]);
+			})
+			.catch(userErrorHandler(req, res));
+	});
 
-    router.get('/text/:name', isLoggedIn, (req, res, next) => {
-        fs.readFile('src/static/' + req.params.name + '.txt', 'utf8', function (err, data) {
-            if (err) res.sendStatus(404);
-            if (req.params.name === 'start') {
-                dGenUtils.getClue(req.user)
-                    .then((resClue) => {
-                        data = data.replace('<CLUE>', resClue.clue[0]);
-                        res.send(data);
-                    })
-                    .catch(userErrorHandler(req, res));
+	router.get('/text/:name', isLoggedIn, (req, res, next) => {
+		fs.readFile('src/static/' + req.params.name + '.txt', 'utf8', function (err, data) {
+			if (err) res.sendStatus(404);
+			if (req.params.name === 'start') {
+				dGenUtils.getClue(req.user)
+					.then((resClue) => {
+						data = data.replace('<CLUE>', resClue.clue[0]);
+						res.send(data);
+					})
+					.catch(userErrorHandler(req, res));
 
-            } else {
-                res.send(data);
-            }
-        });
-    });
+			} else {
+				res.send(data);
+			}
+		});
+	});
 
-    router.get('/generate', isLoggedIn, (req, res) => {
-        dGenUtils.generate(req.user)
-            .then(() => res.render('welcome'))
-            .catch(userErrorHandler(req, res));
-    });
+	router.get('/generate', isLoggedIn, (req, res) => {
+		dGenUtils.generate(req.user)
+			.then(() => res.render('welcome'))
+			.catch(userErrorHandler(req, res));
+	});
 
-    router.get('/maze-id', isLoggedIn, (req, res) => {
-        res.send(req.user.tikalId);
-    });
+	router.get('/maze-id', isLoggedIn, (req, res) => {
+		res.send(req.user.tikalId);
+	});
 
-    router.get('/maze/:mazeId/currentRoom', (req, res) => {
-        dGenUtils.getCurrentRoom(req.params.mazeId)
-            .then(description => res.send(description))
-            .catch(mazeErrorHandler(req, res));
-    });
+	router.get('/maze/:mazeId/currentRoom', (req, res) => {
+		dGenUtils.getCurrentRoom(req.params.mazeId)
+			.then(description => res.send(description))
+			.catch(mazeErrorHandler(req, res));
+	});
 
-    router.get('/maze/:mazeId/reset', (req, res) => {
-        dGenUtils.reset(req.params.mazeId)
-            .then(description => res.send(description))
-            .catch(mazeErrorHandler(req, res));
-    });
+	router.get('/maze/:mazeId/reset', (req, res) => {
+		dGenUtils.reset(req.params.mazeId)
+			.then(description => res.send(description))
+			.catch(mazeErrorHandler(req, res));
+	});
 
-    router.get('/maze/:mazeId/room/:roomId/describe', (req, res) => {
-        dGenUtils.getRoomDescription(req.params.mazeId, req.params.roomId)
-            .then(description => res.send(description))
-            .catch(mazeErrorHandler(req, res));
-    });
+	router.get('/maze/:mazeId/describe', (req, res) => {
+		dGenUtils.getRoomDescription(req.params.mazeId)
+			.then(description => res.send(description))
+			.catch(mazeErrorHandler(req, res));
+	});
 
-    router.get('/maze/:mazeId/room/:roomId/exits', (req, res) => {
-        dGenUtils.getRoomExits(req.params.mazeId, req.params.roomId)
-            .then(exits => res.send(exits))
-            .catch(mazeErrorHandler(req, res));
-    });
+	router.get('/maze/:mazeId/exits', (req, res) => {
+		dGenUtils.getRoomExits(req.params.mazeId)
+			.then(exits => res.send(exits))
+			.catch(mazeErrorHandler(req, res));
+	});
 
-    router.get('/maze/:mazeId/room/:roomId/exit/:direction', (req, res) => {
-        dGenUtils.exitRoom(req.params.mazeId, req.params.roomId, req.params.direction)
-            .then(newRoomId => res.send(newRoomId))
-            .catch(mazeErrorHandler(req, res));
-    });
+	router.get('/maze/:mazeId/exit/:direction', (req, res) => {
+		dGenUtils.exitRoom(req.params.mazeId, req.params.direction)
+			.then(newRoomId => res.send(newRoomId))
+			.catch(mazeErrorHandler(req, res));
+	});
 
-    router.get('/maze/:mazeId/validate/:hash', (req, res) => {
-        dGenUtils.validate(req.params.mazeId, req.params.hash)
-            .then(verified => res.send(verified))
-            .catch(mazeErrorHandler(req, res));
-    });
+	router.get('/maze/:mazeId/validate/:hash', (req, res) => {
+		dGenUtils.validate(req.params.mazeId, req.params.hash)
+			.then(verified => res.send(verified))
+			.catch(mazeErrorHandler(req, res));
+	});
 
-    router.get('/logout', function (req, res) {
-        req.logout();
-        res.redirect('/');
-    });
+	router.get('/logout', function (req, res) {
+		req.logout();
+		res.redirect('/');
+	});
 
-    router.get('/:clue', isLoggedIn, (req, res, next) => {
-        // default route that is not '/'
-        // this MUST be the last route defined, otherwise it'll override other routes.
-        // it will either redirect to '/generate' if the clue is correct
-        // or redirect back to '/start' since the clue is incorrect
-        dGenUtils.getClue(req.user)
-            .then(resClue => {
-                if (req.params.clue === resClue.clue.join('')) {
-                    res.redirect('/generate');
-                } else {
-                    res.redirect('/start');
-                }
-            })
-            .catch(userErrorHandler(req, res));
-    });
+	router.get('/:clue', isLoggedIn, (req, res, next) => {
+		// default route that is not '/'
+		// this MUST be the last route defined, otherwise it'll override other routes.
+		// it will either redirect to '/generate' if the clue is correct
+		// or redirect back to '/start' since the clue is incorrect
+		dGenUtils.getClue(req.user)
+			.then(resClue => {
+				if (req.params.clue === resClue.clue.join('')) {
+					res.redirect('/generate');
+				} else {
+					res.redirect('/start');
+				}
+			})
+			.catch(userErrorHandler(req, res));
+	});
 
-    router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
-    router.get('/auth/google/callback', passport.authenticate('google', authCallbackObj));
+	router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+	router.get('/auth/google/callback', passport.authenticate('google', authCallbackObj));
 
-    router.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
-    router.get('/auth/facebook/callback', passport.authenticate('facebook', authCallbackObj));
+	router.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
+	router.get('/auth/facebook/callback', passport.authenticate('facebook', authCallbackObj));
 
-    router.get('/auth/github', passport.authenticate('github', {scope: 'user:email'}));
-    router.get('/auth/github/callback', passport.authenticate('github', authCallbackObj));
+	router.get('/auth/github', passport.authenticate('github', {scope: 'user:email'}));
+	router.get('/auth/github/callback', passport.authenticate('github', authCallbackObj));
 
-    function isLoggedIn(req, res, next) {
-        if (req.isAuthenticated()) {
-            return next();
-        }
+	function isLoggedIn(req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		}
 
-        res.redirect('/');
-    }
+		res.redirect('/');
+	}
 
-    return router;
+	return router;
 };
--- src/dungeon-generator/index.js
+++ src/dungeon-generator/index.js
@@ -4,312 +4,305 @@ const _ = require('lodash');
 const quests = require('./quests').quests;
 
 function getTikalId(user) {
-    const tikalId = `${user.provider}-${user.id}`;
-    const encoded = Buffer.from(tikalId, 'utf8');
-    return encoded.toString('base64');
+	const tikalId = `${user.provider}-${user.id}`;
+	const encoded = Buffer.from(tikalId, 'utf8');
+	return encoded.toString('base64');
 }
 
 function generateClue() {
-    const sample = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
-    return [
-        _.join(_.sampleSize(sample, 8), ''),
-        _.join(_.sampleSize(sample, 8), ''),
-        _.join(_.sampleSize(sample, 8), ''),
-    ];
+	const sample = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+	return [
+		_.join(_.sampleSize(sample, 8), ''),
+		_.join(_.sampleSize(sample, 8), ''),
+		_.join(_.sampleSize(sample, 8), ''),
+	];
 }
 
 module.exports.getClue = (user) => {
-    user.tikalId = getTikalId(user);
+	user.tikalId = getTikalId(user);
 
-    return new Promise((resolve, reject) => {
-        db.getDungeon(user.tikalId)
-            .then(doc => {
-                if (doc) {
-                    // doc already exists, so it must have clue
-                    resolve({
-                        key: user.tikalId,
-                        clue: doc.clue
-                    });
+	return new Promise((resolve, reject) => {
+		db.getDungeon(user.tikalId)
+			.then(doc => {
+				if (doc) {
+					// doc already exists, so it must have clue
+					resolve({
+						key: user.tikalId,
+						clue: doc.clue
+					});
 
-                } else {
-                    // create new doc
-                    const newDoc = {
-                        key: user.tikalId,
-                        clue: generateClue(),
-                        user: user,
-                    };
-                    return db.saveDungeon(newDoc);
-                }
+				} else {
+					// create new doc
+					const newDoc = {
+						key: user.tikalId,
+						clue: generateClue(),
+						user: user,
+					};
+					return db.saveDungeon(newDoc);
+				}
 
-            })
-            .then(res => resolve(res))
-            .catch(err => reject(err));
-    });
+			})
+			.then(res => resolve(res))
+			.catch(err => reject(err));
+	});
 
 };
 
 module.exports.generate = (user) => {
-    user.tikalId = getTikalId(user);
+	user.tikalId = getTikalId(user);
 
-    return new Promise((resolve, reject) => {
-        db.getDungeon(user.tikalId)
-            .then(doc => {
-                if (!doc) {
-                    // at this point doc must exist with clue
-                    reject(new Error('no doc found in DB!'));
-                }
+	return new Promise((resolve, reject) => {
+		db.getDungeon(user.tikalId)
+			.then(doc => {
+				if (!doc) {
+					// at this point doc must exist with clue
+					reject(new Error('no doc found in DB!'));
+				}
 
-                if (doc.dungeon) {
-                    // if doc already has dungeon, no need to generate - just return it.
-                    resolve({
-                        tikalId: doc.key,
-                        firstRoomId: doc.dungeon[0].id
-                    });
+				if (doc.dungeon) {
+					// if doc already has dungeon, no need to generate - just return it.
+					resolve({
+						tikalId: doc.key,
+						firstRoomId: doc.dungeon[0].id
+					});
 
-                } else {
-                    // create the dungeon and update db
-                    const dungeon = new Dungeon().generate(_.cloneDeep(quests)).persistAndReset();
-                    const newDoc = {
-                        key: user.tikalId,
-                        clue: doc.clue,
-                        hash: dungeon.hash,
-                        numOfValidationTries: 0,
-                        numOfResets: 0,
-                        lastVisitedRoomId: [dungeon.dungeon[0].id],
-                        dungeon: dungeon.dungeon,
-                        user: doc.user,
-                    };
+				} else {
+					// create the dungeon and update db
+					const dungeon = new Dungeon().generate(_.cloneDeep(quests)).persistAndReset();
+					const newDoc = {
+						key: user.tikalId,
+						clue: doc.clue,
+						hash: dungeon.hash,
+						numOfValidationTries: 0,
+						numOfResets: 0,
+						lastVisitedRoomId: [dungeon.dungeon[0].id],
+						dungeon: dungeon.dungeon,
+						user: doc.user,
+					};
 
-                    return db.updateDungeon(newDoc);
-                }
-            })
-            .then(generateObj => resolve(generateObj))
-            .catch(err => reject(err));
-    });
+					return db.updateDungeon(newDoc);
+				}
+			})
+			.then(generateObj => resolve(generateObj))
+			.catch(err => reject(err));
+	});
 };
 
 module.exports.getCurrentRoom = (key) => {
-    return new Promise((resolve, reject) => {
-        db.getDungeon(key)
-            .then(doc => {
-                if (!doc || !doc.lastVisitedRoomId) {
-                    reject(new Error(`Dungeon not found for key ${key}`));
-                }
+	return new Promise((resolve, reject) => {
+		db.getDungeon(key)
+			.then(doc => {
+				if (!doc || !doc.lastVisitedRoomId) {
+					reject(new Error(`Dungeon not found for key ${key}`));
+				}
 
-                resolve({
-                    currentRoomId: _.last(doc.lastVisitedRoomId)
-                });
+				resolve({
+					currentRoomId: _.last(doc.lastVisitedRoomId)
+				});
 
-            })
-            .catch(err => reject(err));
-    });
+			})
+			.catch(err => reject(err));
+	});
 };
 
 module.exports.reset = (key) => {
-    return new Promise((resolve, reject) => {
-        db.reset(key)
-            .then(doc => {
-                if (!doc) {
-                    reject(new Error(`Dungeon not found for key ${key}`));
-                }
+	return new Promise((resolve, reject) => {
+		db.reset(key)
+			.then(doc => {
+				if (!doc) {
+					reject(new Error(`Dungeon not found for key ${key}`));
+				}
 
-                resolve({
-                    currentRoomId: doc.lastVisitedRoomId
-                });
-            })
-            .catch(err => reject(err));
-    });
+				resolve({
+					currentRoomId: doc.lastVisitedRoomId
+				});
+			})
+			.catch(err => reject(err));
+	});
 };
 
 module.exports.validate = (key, hash) => {
-    return new Promise((resolve, reject) => {
-        db.getDungeon(key)
-            .then(doc => {
-                if (!doc) {
-                    reject(new Error(`Dungeon not found for key ${key}`));
-                }
+	return new Promise((resolve, reject) => {
+		db.getDungeon(key)
+			.then(doc => {
+				if (!doc) {
+					reject(new Error(`Dungeon not found for key ${key}`));
+				}
 
-                if (doc.numOfValidationTries >= 20) {
-                    reject(new Error('You have reached the limit of validation tries!'));
+				if (doc.numOfValidationTries >= 20) {
+					reject(new Error('You have reached the limit of validation tries!'));
 
-                } else {
-                    db.updateNumberOfTries(key)
-                        .then(number => resolve({validated: hash === doc.hash}))
-                        .catch(err => reject(err));
-                }
+				} else {
+					db.updateNumberOfTries(key)
+						.then(number => resolve({validated: hash === doc.hash}))
+						.catch(err => reject(err));
+				}
 
-            })
-            .catch(err => reject(err));
-    });
+			})
+			.catch(err => reject(err));
+	});
 };
 
-module.exports.getRoom = (key, roomId) => {
-    return new Promise((resolve, reject) => {
-        db.getDungeon(key)
-            .then(doc => {
-                if (!doc) {
-                    reject(new Error(`Dungeon not found for key ${key}`));
-                }
+module.exports.getRoom = (key) => {
+	return new Promise((resolve, reject) => {
+		db.getDungeon(key)
+			.then(doc => {
+				if (!doc) {
+					reject(new Error(`Dungeon not found for key ${key}`));
+				}
 
-                if (!doc.dungeon[roomId]) {
-                    reject(new Error(`Room ${roomId} not found in Dungeon`));
-                }
+				const roomId = Number(_.last(doc.lastVisitedRoomId));
 
-                const lastVisitedRoom = Number(_.last(doc.lastVisitedRoomId));
-
-                if (lastVisitedRoom !== Number(roomId)) {
-                    reject(new Error(`You are currently in room ${lastVisitedRoom}, you cannot make this operation on room ${roomId}, you are not there!`));
-                }
-
-                resolve({
-                    room: doc.dungeon[roomId],
-                    items: doc.dungeon.items,
-                    lastVisitedRooms: doc.lastVisitedRoomId,
-                });
-            })
-            .catch(err => reject(err));
-    });
+				resolve({
+					room: doc.dungeon[roomId],
+					items: doc.dungeon.items,
+					lastVisitedRooms: doc.lastVisitedRoomId,
+				});
+			})
+			.catch(err => reject(err));
+	});
 };
 
 function doAction(desc, room) {
-    desc.quest = {
-        questId: room.item.questId,
-        itemId: room.item.itemId,
-        pickedUpItem: true,
-        description: `${room.item.desc} ${room.item.action}`
-    };
+	desc.quest = {
+		questId: room.item.questId,
+		itemId: room.item.itemId,
+		pickedUpItem: true,
+		description: `${room.item.desc} ${room.item.action}`
+	};
 }
 
 function doActionPrereq(desc, room) {
-    delete desc.hashLetter;
-    desc.quest = {
-        questId: room.item.questId,
-        itemId: room.item.itemId,
-        pickedUpItem: false,
-        description: `${room.item.desc} ${room.item.actionPrereqNotMet}`
-    };
+	delete desc.hashLetter;
+	desc.quest = {
+		questId: room.item.questId,
+		itemId: room.item.itemId,
+		pickedUpItem: false,
+		description: `${room.item.desc} ${room.item.actionPrereqNotMet}`
+	};
 }
 
-module.exports.getRoomDescription = (key, roomId) => {
-    // auto pick up -> update db that player collected the item
-    // if it is the last item in the quest and all prereqs are valid, also return the tikalTag
-    return new Promise((resolve, reject) => {
-        this.getRoom(key, roomId)
-            .then(doc => {
-                const room = doc.room;
-                const items = doc.items;
-                const lastVisitedRooms = doc.lastVisitedRooms;
+module.exports.getRoomDescription = (key) => {
+	// auto pick up -> update db that player collected the item
+	// if it is the last item in the quest and all prereqs are valid, also return the tikalTag
+	return new Promise((resolve, reject) => {
+		this.getRoom(key)
+			.then(doc => {
+				const room = doc.room;
+				const items = doc.items;
+				const lastVisitedRooms = doc.lastVisitedRooms;
 
-                let desc = {
-                    hashLetter: room.tikalTag
-                };
+				let desc = {
+					roomId: doc.room.id,
+					hashLetter: room.tikalTag
+				};
 
-                if (room.item) {
-                    if (!room.item.prereqObj) {
-                        // no prereq, just do the item action (update db) and give the hash.
-                        doAction(desc, room);
+				if (room.item) {
+					if (!room.item.prereqObj) {
+						// no prereq, just do the item action (update db) and give the hash.
+						doAction(desc, room);
 
-                        if (room.item.encoding) {
-                            delete desc.hashLetter;
-                        }
+						if (room.item.encoding) {
+							delete desc.hashLetter;
+						}
 
-                        if (!_.find(items, {'itemId': room.item.itemId})) {
-                            db.updateItem(key, room.item).then((items) => {
-                                resolve({description: desc});
-                            });
-                        } else {
-                            resolve({description: desc});
-                        }
-                    }
+						if (!_.find(items, {'itemId': room.item.itemId})) {
+							db.updateItem(key, room.item).then((items) => {
+								resolve({description: desc});
+							});
+						} else {
+							resolve({description: desc});
+						}
+					}
 
-                    if (room.item.prereqObj) {
-                        // currently, only supports `minUniqueRoomsVisited`, `prereqId`
-                        if (room.item.prereqObj.minUniqueRoomsVisited) {
-                            const uniqueVisitedRooms = _.uniq(lastVisitedRooms).length;
-                            if (uniqueVisitedRooms >= room.item.prereqObj.minUniqueRoomsVisited) {
-                                // prereq met, pick it up and give hash.
-                                doAction(desc, room);
+					if (room.item.prereqObj) {
+						// currently, only supports `minUniqueRoomsVisited`, `prereqId`
+						if (room.item.prereqObj.minUniqueRoomsVisited) {
+							const uniqueVisitedRooms = _.uniq(lastVisitedRooms).length;
+							if (uniqueVisitedRooms >= room.item.prereqObj.minUniqueRoomsVisited) {
+								// prereq met, pick it up and give hash.
+								doAction(desc, room);
 
-                                if (!_.find(items, {'itemId': room.item.itemId})) {
-                                    db.updateItem(key, room.item).then((items) => {
-                                        resolve({description: desc});
-                                    });
-                                } else {
-                                    resolve({description: desc});
-                                }
-                            } else {
-                                // prereq not met, do actionPrereqNotMet and remove hash
-                                doActionPrereq(desc, room);
+								if (!_.find(items, {'itemId': room.item.itemId})) {
+									db.updateItem(key, room.item).then((items) => {
+										resolve({description: desc});
+									});
+								} else {
+									resolve({description: desc});
+								}
+							} else {
+								// prereq not met, do actionPrereqNotMet and remove hash
+								doActionPrereq(desc, room);
 
-                                resolve({description: desc});
-                            }
-                        }
+								resolve({description: desc});
+							}
+						}
 
-                        if (room.item.prereqObj.prereqId) {
-                            // if prereq, check if prereq already done.
-                            // yes? do action. no? do actionPrereqNotMet
-                            if (_.find(items, {'itemId': room.item.prereqObj.prereqId})) {
-                                // prereq met, pick it up and give hash.
-                                doAction(desc, room);
+						if (room.item.prereqObj.prereqId) {
+							// if prereq, check if prereq already done.
+							// yes? do action. no? do actionPrereqNotMet
+							if (_.find(items, {'itemId': room.item.prereqObj.prereqId})) {
+								// prereq met, pick it up and give hash.
+								doAction(desc, room);
 
-                                if (!_.find(items, {'itemId': room.item.itemId})) {
-                                    db.updateItem(key, room.item).then((items) => {
-                                        resolve({description: desc});
-                                    });
-                                } else {
-                                    resolve({description: desc});
-                                }
+								if (!_.find(items, {'itemId': room.item.itemId})) {
+									db.updateItem(key, room.item).then((items) => {
+										resolve({description: desc});
+									});
+								} else {
+									resolve({description: desc});
+								}
 
-                            } else {
-                                // prereq not met, do actionPrereqNotMet and remove hash
-                                doActionPrereq(desc, room);
+							} else {
+								// prereq not met, do actionPrereqNotMet and remove hash
+								doActionPrereq(desc, room);
 
-                                resolve({description: desc});
-                            }
-                        }
+								resolve({description: desc});
+							}
+						}
 
-                    }
+					}
 
-                } else {
-                    resolve({description: desc});
-                }
+				} else {
+					resolve({description: desc});
+				}
 
-            })
-            .catch(err => reject(err));
-    });
+			})
+			.catch(err => reject(err));
+	});
 };
 
-module.exports.getRoomExits = (key, roomId) => {
-    return new Promise((resolve, reject) => {
-        this.getRoom(key, roomId)
-            .then(doc => {
-                const exits = doc.room.exits.map((exit) => {
-                    return exit.direction;
-                });
+module.exports.getRoomExits = (key) => {
+	return new Promise((resolve, reject) => {
+		this.getRoom(key)
+			.then(doc => {
+				const exits = doc.room.exits.map((exit) => {
+					return exit.direction;
+				});
 
-                resolve({exits: exits});
-            })
-            .catch(err => reject(err));
-    });
+				resolve({exits: exits});
+			})
+			.catch(err => reject(err));
+	});
 };
 
-module.exports.exitRoom = (key, roomId, direction) => {
-    return new Promise((resolve, reject) => {
-        this.getRoom(key, roomId)
-            .then(doc => {
-                const nextRoomIds = doc.room.exits
-                    .filter(exit => exit.direction === parseInt(direction))
-                    .map(exit => exit.targetRoomId);
+module.exports.exitRoom = (key, direction) => {
+	return new Promise((resolve, reject) => {
+		this.getRoom(key)
+			.then(doc => {
+				const nextRoomIds = doc.room.exits
+					.filter(exit => exit.direction === parseInt(direction))
+					.map(exit => exit.targetRoomId);
 
-                if (nextRoomIds.length !== 1) {
-                    reject(`Room ${roomId} does not have an exit at direction ${direction}`);
+				if (nextRoomIds.length !== 1) {
+					reject(new Error(`Room ${doc.room.id} does not have an exit at direction ${direction}`));
 
-                } else {
-                    return db.updateLastVisitedRoom(key, nextRoomIds[0]);
+				} else {
+					return db.updateLastVisitedRoom(key, nextRoomIds[0]);
 
-                }
-            })
-            .then(newRoomId => resolve({newRoomId: newRoomId}))
-            .catch(err => reject(err));
-    });
+				}
+			})
+			.then(newRoomId => resolve({newRoomId: newRoomId}))
+			.catch(err => reject(err));
+	});
 };
