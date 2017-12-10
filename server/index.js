acc3d6908dacba5414213af18d96b8adcf8fef73
Alex Raihelgaus <alex.raihel@gmail.com>
2017-12-10 14:38:05 +0200

    fixed collision betwenn mocha and server

--- server/index.js
+++ server/index.js
@@ -65,13 +65,21 @@ app.get('/start', (req, res, next) => {
 	}
 });
 
-app.listen(port, err => {
-	if (err) {
-		res.render('error');
-	}
+// module.parent check is required to support mocha watch
+// src: https://github.com/mochajs/mocha/issues/1912
+if (!module.parent) {
 
-	console.log(`Listening on port ${port}`);
-});
+	app.listen(port, err => {
+		if (err) {
+			res.render('error');
+		}
+
+		console.log(`Listening on port ${port}`);
+	});
+
+}
+
+
 
 module.exports = app;
 
--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const app = require('./../server/index');
 
 mongoose.Promise = global.Promise;
 
